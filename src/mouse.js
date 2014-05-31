3f8efb5166a180bf4a84bb72c098505686359423
Josh Marchán <sykopomp@sykosomatic.org>
2014-05-31 02:06:47 -0700

    Consistifying on heavy fat arrows

--- src/mouse.js
+++ src/mouse.js
@@ -9,44 +9,32 @@ export var mousedown = domStream("mousedown");
 
 export var click = domStream("click");
 
-export var clicks = function(target) {
-  return click(target).map(function(ev) {
-    return {
+export var clicks = target =>
+  click(target).map(ev => ({
+    x: target ? ev.offsetX : ev.pageX,
+    y: target ? ev.offsetY : ev.pageY
+  }));
+
+export var deltas = target =>
+  position(target).diff(null, (a, b) =>
+    a ? {x: b.x - a.x, y: b.y - a.y} : {x: 0, y: 0})
+  .toEventStream();
+
+export var position = target =>
+  mousemove(target)
+    .map(ev => ({
       x: target ? ev.offsetX : ev.pageX,
       y: target ? ev.offsetY : ev.pageY
-    };
-  });
-};
+    })).toProperty();
 
-export var deltas = function(target) {
-  return position(target).diff(null, function(a, b) {
-    return a ? {x: b.x - a.x, y: b.y - a.y} : {x: 0, y: 0};
-  }).toEventStream();
-};
+export var isUp = (upTarget, downTarget) =>
+  mouseup(upTarget).map(true)
+  .merge(mousedown(downTarget).map(false))
+  .toProperty();
 
-export var position = function(target) {
-  return mousemove(target)
-    .map(function(ev) {
-      return {
-        x: target ? ev.offsetX : ev.pageX,
-        y: target ? ev.offsetY : ev.pageY
-      };
-    }).toProperty();
-};
-
-export var isUp = function(upTarget, downTarget) {
-  return mouseup(upTarget).map(true)
-    .merge(mousedown(downTarget).map(false))
-    .toProperty();
-};
-
-export var isDown = function(downTarget, upTarget) {
-  return isUp(upTarget, downTarget).not();
-};
+export var isDown = (downTarget, upTarget) => isUp(upTarget, downTarget).not();
 
 function domStream(name) {
   let docStream = $(document).asEventStream(name);
-  return function(target) {
-    return target ? $(target).asEventStream(name) : docStream;
-  };
+  return target => target ? $(target).asEventStream(name) : docStream;
 }
