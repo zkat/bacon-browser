d53bc1e37e19b5b476a78ed4833f48d8442b2b1e
Josh Marchán <sykopomp@sykosomatic.org>
2014-06-01 18:11:38 -0400

    More Mouse methods, different isUp/isDown, and new isHeld utility

--- src/mouse.js
+++ src/mouse.js
@@ -1,25 +1,168 @@
 module $ from "jquery";
 import "bacon";
 
+/**
+ * Event wrapper for `DOMElement.onmousemove`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mousemove Events
+ */
 export var mousemove = domStream("mousemove");
 
+/**
+ * Event wrapper for `DOMElement.onmousemove`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mousemove Events
+ */
 export var mouseup = domStream("mouseup");
 
+/**
+ * Event wrapper for `DOMElement.onmousedown`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mousedown Events
+ */
 export var mousedown = domStream("mousedown");
 
+/**
+ * Event wrapper for `DOMElement.onmouseenter`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mouseenter Events
+ */
+export var mouseenter = domStream("mouseenter");
+
+/**
+ * Event wrapper for `DOMElement.onmouseleave`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mouseleave Events
+ */
+export var mouseleave = domStream("mouseleave");
+
+/**
+ * Event wrapper for `DOMElement.onmouseover`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mouseover Events
+ */
+export var mouseover = domStream("mouseover");
+
+/**
+ * Event wrapper for `DOMElement.onmouseout`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of mouseout Events
+ */
+export var mouseout = domStream("mouseout");
+
+/**
+ * Creates an event stream that returns a boolean whenever the mouse enters or
+ * leaves the target.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of Boolean
+ */
+export var hover = target =>
+  mouseenter(target).map(true).merge(mouseleave(target).map(false));
+
+/**
+ * Creates a property that is true if the mouse is currently hovering over
+ * `target`. This is simply the Property version of `Mouse.hover()`.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns Property of Boolean
+ */
+export var hovering = target =>
+  hover(target).toProperty();
+
+/**
+ * Event wrapper for `DOMElement.onclick`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of click Events
+ */
 export var click = domStream("click");
 
-export var clicks = target =>
+/**
+ * Event wrapper for `DOMElement.ondblclick`
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of dblclick Events
+ */
+export var dblclick = domStream("dblclick");
+
+/**
+ * Creates an EventStream of coordinates where clicks have occurred. If the
+ * `useOffset` argument is given, the `click` events' `offsetX/offsetY` values
+ * are used, otherwise `pageX/pageY` are given.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ * @param {Boolean} [useOffset=false] - Whether to use `pageX/pageY` (default)
+ * or `offsetX/offsetY` (if flag is true) when creating the stream values.
+ *
+ * @returns EventStream of {x: Int, y: Int}
+ */
+export var clicks = (target, useOffset) =>
   click(target).map(ev => ({
     x: target ? ev.offsetX : ev.pageX,
     y: target ? ev.offsetY : ev.pageY
   }));
 
+/**
+ * Creates an EventStream of mouse movement deltas, based on the preceding
+ * mousemove. The stream values represent the pixel x/y offsets.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns EventStream of {x: Int, y: Int}
+ *
+ * @example
+ * Bacon.Browser.Mouse.deltas().map(".x")
+ *   .log("Mouse moved horizontally this many pixels:");
+ */
 export var deltas = target =>
   position(target).diff(null, (a, b) =>
     a ? {x: b.x - a.x, y: b.y - a.y} : {x: 0, y: 0})
   .toEventStream();
 
+/**
+ * Creates a Property representing the current mouse position. If the
+ * `useOffset` argument is given, the `mousemove` events' `offsetX/offsetY`
+ * values are used. Otherwise `pageX/pageY` are given.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ * @param {Boolean} [useOffset=false] - Whether to use `pageX/pageY` (default)
+ * or `offsetX/offsetY` (if flag is true) when creating the stream values.
+ *
+ * @returns Property of {x: Int, y: Int}
+ */
 export var position = target =>
   mousemove(target)
     .map(ev => ({
@@ -27,12 +170,49 @@ export var position = target =>
       y: target ? ev.offsetY : ev.pageY
     })).toProperty();
 
-export var isUp = (upTarget, downTarget) =>
-  mouseup(upTarget).map(true)
-  .merge(mousedown(downTarget).map(false))
-  .toProperty();
+/**
+ * Creates a Property that is true if the mouse is currently up. If `target` is
+ * given, returns true only when the mouse is hovering over `target` without
+ * being held down.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns Property of Boolean
+ */
+export var isUp = target =>
+  isHeld().not().and(hovering(target));
 
-export var isDown = (downTarget, upTarget) => isUp(upTarget, downTarget).not();
+/**
+ * Creates a Property that is true if the mouse is currently down. If `target`
+ * is given, returns true only when the mouse is hovering over `target` while
+ * being held down.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns Property of Boolean
+ */
+export var isDown = target =>
+  isHeld().and(hovering(target));
+
+/**
+ * Creates a property that is true if the target is being "held" -- meaning, if
+ * the mouse went down on it, and it hasn't been released. The difference
+ * between this property and the one created by `isDown` is that `isDown` will
+ * be 'false' if the mouse leaves the `target`.
+ *
+ * This is usually the function you want for drag-and-drop behaviors.
+ *
+ * @param {EventTarget} [target=document] - Target to watch for events on. This
+ * can be anything `$()` accepts.
+ *
+ * @returns Property of Boolean
+ */
+export var isHeld = target =>
+  mousedown(target).map(true)
+  .merge(mouseup().map(false))
+  .toProperty();
 
 function domStream(name) {
   let docStream = $(document).asEventStream(name);
