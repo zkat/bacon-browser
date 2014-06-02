510af38d90efa5e991bb87e86802f6fa4fa9ad11
Josh Marchán <sykopomp@sykosomatic.org>
2014-06-02 01:24:02 -0400

    Doc tweaks and moving stuff around

--- src/keyboard.js
+++ src/keyboard.js
@@ -8,87 +8,103 @@ import {domStream} from "./util";
 var keydown = domStream("keydown");
 var keyup = domStream("keyup");
 
+/*
+ * EventStream
+ */
+
 /**
- * Stream of keydown keycodes. This is intended for handling key input meant for
+ * Stream of keydown keyCodes. This is intended for handling key input meant for
  * something other than text processing. (detecting Escape, arrow keys, etc.)
- * The keycode values are normalized by jQuery for better cross-browser support.
+ * The keyCode values are normalized by jQuery for better cross-browser support.
  *
- * The `filter` argument will be used to filter which keycodes will actually
- * trigger the event. `filter` can be one of an integer matching the keycode, an
- * array of integers of the possible keycodes, or a function which will receive
- * the keycode as an argument and accept it into the stream if a truthy value is
+ * The `filter` argument will be used to filter which keyCodes will actually
+ * trigger the event. `filter` can be one of an integer matching the keyCode, an
+ * array of integers of the possible keyCodes, or a function which will receive
+ * the keyCode as an argument and accept it into the stream if a truthy value is
  * returned.
+ *
+ * If `useKeyup` is truthy, `keyCodes()` will only fire on the `keyup` event,
+ * instead of the default `keydown`.
  *
  * @param {EventTarget} [target=document] - Target to watch for events on. This
  * can be anything `$()` accepts.
  * @param {Int|[Int]|Function Bool} [filter] - Filter to determine whether to
- * accept a keycode.
+ * accept a keyCode.
  * @param {Boolean} [useKeyUp] - If true, listens for `keyup` events instead of
  * `keydown`
  *
  * If you're interested in handling input for the sake of text, use the built-in
  * `input` event on input/textarea elements.
  *
- * @returns EventStream of keycodes
+ * @returns EventStream of keyCodes
  */
-export var keycodes = (target, filter, useKeyup) =>
+export var keyCodes = (target, filter, useKeyup) =>
   (useKeyup?keyup:keydown)().map(".which").filter(
     !filter ? (x => x) :
     typeof filter === "function" ? filter :
       typeof filter === "number" ? (code => code === filter) :
       (code => ~filter.indexOf(code)));
 
+/*
+ * Property
+ */
+
 /**
  * Creates a Property that is true if a key is currently up and `target` is
  * focused. If `filter` is provided, the property will toggle only when `filter`
- * matches the event keycode. See `Keyboard.keycodes()` for information on
+ * matches the event keyCode. See `Keyboard.keyCodes()` for information on
  * `filter`.
  *
  * @param {EventTarget} [target=document] - Target to watch for events on. This
  * can be anything `$()` accepts.
  * @param {Int|[Int]|Function Bool} [filter] - Filter to determine whether to
- * accept a keycode.
+ * accept a keyCode.
  *
  * @returns Property of Boolean
  */
 export var isUp = (target, filter) =>
-  keycodes(target, filter).map(false)
-  .merge(keycodes(target, filter, true).map(true))
+  keyCodes(target, filter).map(false)
+  .merge(keyCodes(target, filter, true).map(true))
   .skipDuplicates()
   .toProperty(true);
 
 /**
  * Creates a Property that is true if a key is currently down and `target` is
  * focused. If `filter` is provided, the property will toggle only when `filter`
- * matches the event keycode. See `Keyboard.keycodes()` for information on
+ * matches the event keyCode. See `Keyboard.keyCodes()` for information on
  * `filter`.
  *
  * @param {EventTarget} [target=document] - Target to watch for events on. This
  * can be anything `$()` accepts.
  * @param {Int|[Int]|Function Bool} [filter] - Filter to determine whether to
- * accept a keycode.
+ * accept a keyCode.
  *
  * @returns Property of Boolean
  */
 export var isDown = (target, filter) => isUp(target, filter).not();
 
 /**
- * Creates a Property whose value is an array of the keycodes currently held
+ * Alias for `Keyboard.isDown()`
+ */
+export var isHeld = isDown;
+
+/**
+ * Creates a Property whose value is an array of the keyCodes currently held
  * down, if `target` is in focus. If `filter` is provided, the property will
- * toggle only when `filter` matches the event keycode. See
- * `Keyboard.keycodes()` for information on `filter`.
+ * toggle only when `filter` matches the event keyCode. See
+ * `Keyboard.keyCodes()` for information on `filter`.
  *
  * @param {EventTarget} [target=document] - Target to watch for events on. This
  * can be anything `$()` accepts.
  * @param {Int|[Int]|Function Bool} [filter] - Filter to determine whether to
- * accept a keycode.
+ * accept a keyCode.
  *
  * @returns Property of [Int]
  */
 export var held = (target, filter) => {
   let _acc = [];
-  return keycodes(target, filter).doAction((code) => _acc[code] = true)
-    .merge(keycodes(target, filter, true).doAction((code) => delete _acc[code]))
+  return keyCodes(target, filter).doAction((code) => _acc[code] = true)
+    .merge(keyCodes(target, filter, true).doAction((code) => delete _acc[code]))
            .map(() => Object.keys(_acc).map(x => +x))
            .skipDuplicates((a, b) => a.length === b.length);
 };
--- src/window.js
+++ src/window.js
@@ -4,6 +4,7 @@ import {constantly} from "./util";
 
 let $window = $(window);
 
+// Sort of cribbed from CanJS :)
 if (window.history && window.history.pushState) {
   let oldMethods = {};
   ["pushState", "replaceState"].forEach(function(method) {
@@ -24,6 +25,10 @@ var resize = constantly($window.asEventStream("resize"));
 var hashchanged = constantly($window.asEventStream("hashchanged"));
 var popstate = constantly($window.asEventStream("popstate"));
 
+/*
+ * EventStream
+ */
+
 /**
  * Special event that fires whenever the current history state is set, whether
  * from `history.pushState`, `history.replaceState`, or by anything that
@@ -34,62 +39,6 @@ var popstate = constantly($window.asEventStream("popstate"));
 export var statechanged = constantly(
   $window.asEventStream("___bacon-browser-state___",(_,s) => s)
     .merge(popstate().map(".originalEvent.state")));
-
-/**
- * Property that represents the latest value of `window.location.hash`. Updates
- * on `window.onhashchanged`.
- *
- * @returns Property of `window.location.hash`
- */
-export var hash = () =>
-  hashchanged()
-  .map(()=>window.location.hash)
-  .toProperty(window.location.hash);
-
-/**
- * Property that represents the latest value of `window.location`. Updates
- * whenever the URL changes either from a `hashchanged` or using the `history`
- * API, if available.
- *
- * @returns Property of `window.location`
- */
-export var location = ()=>
-  hashchanged()
-  .merge(statechanged())
-  .map(()=>window.location)
-  .toProperty(window.location);
-
-/**
- * Property that represents the current `history` state. Updates whenever
- * `history.pushState` or `history.replaceState` are called, or when anything
- * triggers `window.onpopstate`.
- *
- * @returns Property of history state
- */
-export var state = ()=>statechanged().toProperty();
-
-/**
- * Property of the current `window` outer dimensions, in pixels.
- *
- * @returns Property of {width: Int, height: Int}
- */
-export var dimensions = ()=>resize()
-  .map(()=>({width: $window.outerWidth(), height: $window.outerHeight()}))
-  .toProperty({width: $window.outerWidth(), height: $window.outerHeight()});
-
-/**
- * Property that represents the current window height.
- *
- * @returns Property of window's outer height
- */
-export var height = ()=>dimensions().map(".height");
-
-/**
- * Property that represents the current window width.
- *
- * @returns Property of window's outer width
- */
-export var width = ()=>dimensions().map(".width");
 
 /**
  * Event stream that fires whenever a browser animation frame is available. The
@@ -116,3 +65,59 @@ export var animationFrames = constantly(bacon.fromBinder(function(sink) {
     }
   };
 }));
+
+/*
+ * Property
+ */
+
+/**
+ * The latest value of `window.location`. Updates whenever the URL changes
+ * either from a `hashchanged` or using the `history` API, if available.
+ *
+ * @returns Property of `window.location`
+ */
+export var location = ()=>
+  hashchanged()
+  .merge(statechanged())
+  .map(()=>window.location)
+  .toProperty(window.location);
+
+/**
+ * The latest value of `window.location.hash`. Updates whenever the URL changes
+ * either from a `hashchanged` or using the `history` API, if available.
+ *
+ * @returns Property of `window.location.hash`
+ */
+export var hash = () => location().map(".hash");
+
+/**
+ * The current `history` state. Updates whenever `history.pushState` or
+ * `history.replaceState` are called, or when anything triggers
+ * `window.onpopstate`.
+ *
+ * @returns Property of history state
+ */
+export var state = () => statechanged().toProperty();
+
+/**
+ * The current `window` outer dimensions, in pixels.
+ *
+ * @returns Property of {width: Int, height: Int}
+ */
+export var dimensions = () => resize()
+  .map(()=>({width: $window.outerWidth(), height: $window.outerHeight()}))
+  .toProperty({width: $window.outerWidth(), height: $window.outerHeight()});
+
+/**
+ * The current window height.
+ *
+ * @returns Property of window's outer height
+ */
+export var height = ()=>dimensions().map(".height");
+
+/**
+ * The current window width.
+ *
+ * @returns Property of window's outer width
+ */
+export var width = ()=>dimensions().map(".width");
