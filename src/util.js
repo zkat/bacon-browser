732f91ecc15ed057c7c914c63512bb83417613a4
Josh Marchán <sykopomp@sykosomatic.org>
2014-06-01 21:04:17 -0400

    Moved domStream to shared utils

--- src/util.js
+++ src/util.js
@@ -1,3 +1,9 @@
 export function constantly(x) {
   return ()=>x;
 }
+
+
+export function domStream(name) {
+  let docStream = $(document).asEventStream(name);
+  return target => target ? $(target).asEventStream(name) : docStream;
+}
